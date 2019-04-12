f96f2d4a90266281437a4d912b05a7a99c2e4268
nanilev <dianuski99@gmail.com>
2019-04-12 11:07:25 +0100

    added methods to verify the length of the title

---
+++ BookManager/Book.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookManager
+{
+    class Book
+    {
+        private string title;
+        private string author;
+        private int pages;
+        private int wordCount;
+
+        public Book (string title, string author)
+        {
+            this.title = title;
+            this.author = author;
+        }
+
+        public string GetTitle()
+        {
+            return title;
+        }
+
+        public void SetTitle(string title)
+        {
+            if (this.title != null && this.title.Length > 0)
+                this.title = title;
+        }
+
+        public int GetTitleLength(string title)
+        {
+            return this.title.Length;
+        }
+
+        public void AssignWordCountFromText(string text)
+        {
+            wordCount = text.Split(' ').Length;
+        }
+    }
+}
